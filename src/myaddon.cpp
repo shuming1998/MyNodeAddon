f4cf8ffac98aa39ec90fb97c9560b8635a37216b
shuming1998 <1005586890@qq.com>
2024-04-17 14:51:40 +0800

    auto calculate offsets by struct member types

--- src/myaddon.cpp
+++ src/myaddon.cpp
@@ -25,6 +25,13 @@
 using namespace llvm;
 using namespace llvm::orc;
 
+// LLVM 相关组件
+ExitOnError ExitOnErr;
+LLVMContext TheContext;
+IRBuilder<> Builder(TheContext);
+std::unique_ptr<Module> TheModule;
+std::unique_ptr<LLJIT> TheJIT;
+
 // IR 结构体，用来保存 IR Struct 的名称和指针
 struct LLVMStruct {
     LLVMStruct() : name(""), LLVMStructType(nullptr) {}
@@ -34,28 +41,6 @@ struct LLVMStruct {
     StructType *LLVMStructType;
 };
 
-// 定义一个通用的函数指针类型
-typedef napi_status (*napi_create_func)(napi_env, ...);
-
-// 建立映射，将生产者对应的结构体名称映射到 IR 结构体
-std::unordered_map<std::string, LLVMStruct> map;
-// N-API 函数指针映射表，将 LLVM 变量类型映射到 Nodejs 创建变量类型的函数指针
-std::unordered_map<llvm::Type::TypeID, napi_create_func> napi_function_map;
-
-// 初始化 N-API 函数指针映射表
-void InitializeNapiFunctionMap(napi_env env) {
-    napi_function_map[llvm::Type::TypeID::IntegerTyID] = reinterpret_cast<napi_create_func>(napi_create_int32);
-    napi_function_map[llvm::Type::TypeID::IntegerTyID] = reinterpret_cast<napi_create_func>(napi_create_int64);
-    napi_function_map[llvm::Type::TypeID::FloatTyID] = reinterpret_cast<napi_create_func>(napi_create_double);
-    napi_function_map[llvm::Type::TypeID::DoubleTyID] = reinterpret_cast<napi_create_func>(napi_create_double);
-
-    //napi_function_map[4] = reinterpret_cast<napi_create_func>(napi_create_uint32);
-    //napi_function_map[5] = reinterpret_cast<napi_create_func>(napi_create_string_utf8);
-    //napi_function_map[6] = reinterpret_cast<napi_create_func>(napi_create_string_latin1);
-    //napi_function_map[7] = reinterpret_cast<napi_create_func>(napi_create_boolean);
-    //napi_function_map[8] = reinterpret_cast<napi_create_func>(napi_create_null);
-    //napi_function_map[9] = reinterpret_cast<napi_create_func>(napi_create_undefined);
-}
 
 // 定义结构体，模拟生产者生成的数据
 struct MyStruct {
@@ -89,18 +74,42 @@ struct MyStruct {
     }
 };
 
-// LLVM 相关组件
-ExitOnError ExitOnErr;
-LLVMContext TheContext;
-IRBuilder<> Builder(TheContext);
-std::unique_ptr<Module> TheModule;
-std::unique_ptr<LLJIT> TheJIT;
+// 包装函数，用于从 void* 转换为 int32_t 并调用 napi_create_int32()
+napi_status create_napi_int32(napi_env env, void* dataPtr, napi_value* result) {
+    int32_t value = *static_cast<int32_t*>(dataPtr);
+    return napi_create_int32(env, value, result);
+}
+
+// 包装函数，用于从 void* 转换为 double 并调用 napi_create_double()
+napi_status create_napi_double(napi_env env, void* dataPtr, napi_value* result) {
+    double value = *static_cast<double*>(dataPtr);
+    return napi_create_double(env, value, result);
+}
+
+// 包装函数，用于从 void* 转换为 float 并调用 napi_create_double()
+napi_status create_napi_float(napi_env env, void* dataPtr, napi_value* result) {
+    float value = *static_cast<float*>(dataPtr);
+    return napi_create_double(env, value, result);
+}
+
+// 建立映射表，将生产者对应的结构体名称映射到 IR 结构体
+std::unordered_map<std::string, LLVMStruct> map;
+// 建立映射表，将IR结构体名称映射到其成员名称列表
+std::unordered_map<std::string, std::vector<std::string>> structMemberNamesMap;
 
 // json 类型到 IR 结构体成员类型的映射
 std::map<std::string, llvm::Type *> typeMap = {
     {"i32", Builder.getInt32Ty()},
     {"float", Builder.getFloatTy()},
     {"double", Builder.getDoubleTy()}
+};
+
+// llvm type 到 napi_create_func 映射
+std::unordered_map<llvm::Type::TypeID, std::function<napi_status(napi_env, void*, napi_value*)>> napi_function_map = {
+    { llvm::Type::IntegerTyID, create_napi_int32 },
+    { llvm::Type::DoubleTyID, create_napi_double },
+    { llvm::Type::FloatTyID, create_napi_float },
+    // ... 为其他LLVM类型添加映射 ...
 };
 
 napi_value CreateObjectFromMyStruct(napi_env env, napi_callback_info info) {
@@ -124,14 +133,20 @@ napi_value CreateObjectFromMyStruct(napi_env env, napi_callback_info info) {
     for (const auto &structDef : structsJson) {
         std::string structName = structDef["name"].get<std::string>();
         std::vector<Type *> fieldTypes;
+        std::vector<std::string> fieldNames; // 保存该结构体的成员名称列表
         for (const auto &fieldDef : structDef["fields"]) {
             std::string fieldType = fieldDef["type"].get<std::string>();
+            std::string fieldName = fieldDef["name"].get<std::string>();
             fieldTypes.push_back(typeMap[fieldType]);
+            fieldNames.push_back(fieldName); // 添加成员名称到列表
+            outs() << "field name: " << fieldName << "\n";
         }
 
         StructType *structType = StructType::create(TheContext, fieldTypes, structName);
         LLVMStruct llvmStruct = LLVMStruct(structName, structType);
         map.emplace(structName, llvmStruct);
+        // 将结构体名称和成员名称列表添加到映射表中
+        structMemberNamesMap.emplace(structName, fieldNames);
     }
 
     // 打印一下都保存了哪些结构体
@@ -147,20 +162,42 @@ napi_value CreateObjectFromMyStruct(napi_env env, napi_callback_info info) {
 
     // 拿到生产者数据后，根据结构体名称从 map 中取出 IR 结构体类型
     LLVMStruct llvmMember = map[member.type];
+    // 获取成员名称列表
+    const auto &memberNames = structMemberNamesMap[member.type];
     // 遍历 IR 结构体类型中的成员，并根据类型设置 node 成员的数据
+    size_t currentOffset = 0;
     for (unsigned i = 0, e = llvmMember.LLVMStructType->getNumElements(); i != e; ++i) {
         napi_value nValue;
         auto *ElementType = llvmMember.LLVMStructType->getElementType(i);
-        llvm::Type::TypeID typeID =  ElementType->getTypeID();
-        outs() << "element type id: " << typeID << "\n";
+        llvm::Type::TypeID typeID = ElementType->getTypeID();
 
-        // TODO: 根据类型设置 node 成员：1.需要根据成员类型布局计算偏移量，2.需要获取json中成员名称，代替硬编码 "name" 和 "score"
-        if (ElementType->isIntegerTy()) {
-            napi_create_int32(env, *static_cast<int*>(member.dataPtr), &nValue);
-            napi_set_named_property(env, obj, "name", nValue);
-        } else if (ElementType->isFloatingPointTy()) {
-            napi_create_double(env, *reinterpret_cast<float*>(static_cast<int*>(member.dataPtr) + 1), &nValue);
-            napi_set_named_property(env, obj, "score", nValue);
+        // 查找对应的N-API函数指针
+        auto it = napi_function_map.find(typeID);
+        if (it != napi_function_map.end()) {
+            // 打印一下类型ID
+             outs() << "typeID: " << typeID << "\n";
+            // 打印一下当前偏移量
+            outs() << "currentOffset: " << currentOffset << "\n";
+
+            // 根据类型取得对应的元素大小
+            size_t elementSize = TheModule->getDataLayout().getTypeAllocSize(ElementType);
+            // 计算当前成员的地址
+            void* dataPtrWithOffset = static_cast<char*>(member.dataPtr) + currentOffset;
+
+            // 调用映射表中的函数，它会转换类型并创建相应的N-API值
+            napi_status status = it->second(env, dataPtrWithOffset, &nValue);
+
+            if (status != napi_ok) {
+                // 处理错误情况
+            }
+
+            // 使用成员名称设置JavaScript对象的属性
+            napi_set_named_property(env, obj, memberNames[i].c_str(), nValue);
+
+            // 更新偏移量
+            currentOffset += elementSize;
+        } else {
+            // 错误处理：找不到对应的类型ID
         }
     }
 
@@ -169,9 +206,6 @@ napi_value CreateObjectFromMyStruct(napi_env env, napi_callback_info info) {
 
 napi_value Init(napi_env env, napi_value exports) {
     napi_value fn;
-
-    // 初始化 N-API 函数映射表
-    InitializeNapiFunctionMap(env);
 
     napi_create_function(env, nullptr, 0, CreateObjectFromMyStruct, nullptr, &fn);
     napi_set_named_property(env, exports, "createObjectFromMyStruct", fn);
